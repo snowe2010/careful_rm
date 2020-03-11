678618a12d548b1132ff4f648ca3738c1311f623
Tyler Thrailkill <tyler.b.thrailkill@gmail.com>
2020-03-11 10:36:50 -0600

    Fish shell just sets an alias

--- careful_rm.fish
+++ careful_rm.fish
@@ -4,19 +4,21 @@
 #######################################################################
 
 # Get the alias
-set -g OURDIR "$(dirname $0:A)"
-source "${OURDIR}/careful_rm.alias.sh"
+set -g OURDIR "(dirname $0:A)"
+source "$OURDIR/careful_rm.alias.sh"
 
 # Make a trash aliase that changes with directory
-function chpwd_trash
-    if test -x "${CAREFUL_RM}"
-        set -g TRASH $(python ${CAREFUL_RM} --get-trash)
-        if test [[ "$OSTYPE" == "linux-gnu" ]]
-            set -g TRASH "${TRASH}/files"
-        end
-        hash -d trash="${TRASH}"
-    end
-end
-set -g chpwd_functions ( ${chpwd_functions} chpwd_trash )
-chpwd_trash
-alias trsh="cd \${TRASH}"
+# function chpwd_trash
+#     if test -x "$CAREFUL_RM"
+#         set -g TRASH (python $CAREFUL_RM --get-trash)
+#         if test [[ "$OSTYPE" == "linux-gnu" ]]
+#             set -g TRASH "$TRASH/files"
+#         end
+#         hash -d trash="$TRASH"
+#     end
+# end
+# set -g chpwd_functions ( chpwd_functions chpwd_trash )
+# chpwd_trash
+# alias trsh="cd \$TRASH"
+
+alias rm="$CAREFUL_RM"
